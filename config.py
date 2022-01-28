50b72d13138f88ee03e161d122d9137b04a609c9
hasbai <jsclndnz@outlook.com>
2022-01-28 17:02:38 +0800

    config 不抛异常

--- config.py
+++ config.py
@@ -9,7 +9,10 @@ APP_SECRET = os.environ.get('MP_APP_SECRET')
 PROXY = os.environ.get('HTTP_PROXY')
 AUTHOR = os.environ.get('AUTHOR')
 TOKEN = os.environ.get('TOKEN', '')
-assert APP_ID, 'Environment variable MP_APP_ID must be set.'
-assert APP_SECRET, 'Environment variable MP_APP_SECRET must be set.'
+
+if not APP_ID:
+    print('[E] Environment variable MP_APP_ID must be set.')
+if not APP_SECRET:
+    print('[E] Environment variable MP_APP_SECRET must be set.')
 if not TOKEN:
     print('[W] Environment variable TOKEN not set, should not use in production environment.')

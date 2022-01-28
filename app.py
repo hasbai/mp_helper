09b359a062eb0470ec5921ccf448365077188b64
hasbai <jsclndnz@outlook.com>
2022-01-28 15:22:46 +0800

    服务器

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import asyncio
+from typing import Optional
 
 from bs4 import BeautifulSoup
+from fastapi import FastAPI, Request, UploadFile, Form, HTTPException
+from starlette.responses import JSONResponse
 
+from config import TOKEN
 from mp import Mp
 from utils import markdown_to_html
 
+app = FastAPI()
 
-async def main(publish=True):
-    with open('data/md.md', encoding='utf-8') as f:
-        markdown = f.read()
 
+async def upload_article(markdown: str, publish=True) -> bool:
     # 处理 markdown
     html, metadata = markdown_to_html(markdown)
     async with Mp() as mp:
@@ -33,8 +36,34 @@ async def main(publish=True):
             content=html,
             digest=metadata.get('abstract'),
         )
-        return await mp.publish(media_id) if publish else media_id
+        return await mp.publish(media_id) if publish else bool(media_id)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    if not request.headers.get('Authorization', '') == TOKEN:
+        return JSONResponse(status_code=401)
+    else:
+        response = await call_next(request)
+        return response
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post('/articles', status_code=201)
+async def post_article(file: UploadFile, publish: Optional[bool] = Form(default=True)):
+    markdown = await file.read()
+    markdown = markdown.decode('utf-8')
+    result = await upload_article(markdown=markdown, publish=publish)
+    if not result:
+        raise HTTPException(500)
+    return {'message': 'success'}
 
 
 if __name__ == '__main__':
-    asyncio.run(main(publish=False), debug=True)
+    with open('data/md.md', encoding='utf-8') as f:
+        markdown = f.read()
+    asyncio.run(upload_article(markdown=markdown, publish=False), debug=True)
--- config.py
+++ config.py
@@ -8,5 +8,8 @@ APP_ID = os.environ.get('MP_APP_ID')
 APP_SECRET = os.environ.get('MP_APP_SECRET')
 PROXY = os.environ.get('HTTP_PROXY')
 AUTHOR = os.environ.get('AUTHOR')
+TOKEN = os.environ.get('TOKEN', '')
 assert APP_ID, 'Environment variable MP_APP_ID must be set.'
 assert APP_SECRET, 'Environment variable MP_APP_SECRET must be set.'
+if not TOKEN:
+    print('[W] Environment variable TOKEN not set, should not use in production environment.')
